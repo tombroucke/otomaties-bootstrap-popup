3a0fa9cc29651e1b89394c58391ff73bab1ea910
Tom Broucke <tom@tombroucke.be>
2022-08-12 10:35:34 +0200

    chore(bump): Bump version to 2.1.2

--- otomaties-bootstrap-popup.php
+++ otomaties-bootstrap-popup.php
@@ -7,7 +7,7 @@ namespace Otomaties\BootstrapPopup;
  * Description: Add a configurable popup to your website
  * Author: Tom Broucke
  * Author URI: https://tombroucke.be
- * Version: 2.0.0
+ * Version:           2.1.2
  * License: GPL2
  * Text Domain: otomaties-popup
  * Domain Path: languages
