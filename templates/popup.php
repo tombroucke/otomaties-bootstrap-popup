4dac745df08aa36d71acceebb4d3903ae251230c
Tom Broucke <tom@tombroucke.be>
2022-08-26 14:04:16 +0200

    feat(filter): Add filters for modal div classes

--- templates/popup.php
+++ templates/popup.php
@@ -1,15 +1,15 @@
-<div class="modal fade otomaties-bootstrap-popup" id="popup-<?php echo $popup->getId() ?>" tabindex="-1" role="dialog" aria-labelledby="popup-<?php echo $popup->getId() ?>Label" aria-hidden="true" data-show-once="<?php echo $popup->showOnce(); ?>" data-delay="<?php echo $popup->delay(); ?>" data-hash="<?php echo $popup->hash(); ?>">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="popup-<?php echo $popup->getId() ?>Title"><?php echo $popup->title(); ?></h5>
+<div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_classes', 'modal fade otomaties-bootstrap-popup '); ?>" id="popup-<?php echo $popup->getId() ?>" tabindex="-1" role="dialog" aria-labelledby="popup-<?php echo $popup->getId() ?>Label" aria-hidden="true" data-show-once="<?php echo $popup->showOnce(); ?>" data-delay="<?php echo $popup->delay(); ?>" data-hash="<?php echo $popup->hash(); ?>">
+    <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_dialog_classes', 'modal-dialog'); ?>" role="document">
+        <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_content_classes', 'modal-content'); ?>">
+            <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_header_classes', 'modal-header'); ?>">
+                <h5 class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_title_classes', 'modal-title'); ?>" id="popup-<?php echo $popup->getId() ?>Title"><?php echo $popup->title(); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_body_classes', 'modal-body'); ?>">
                 <?php echo apply_filters('the_content', $popup->content()); ?>
             </div>
             <?php if (!empty($popup->buttons()) || $popup->showCloseButton()) : ?>
-                <div class="modal-footer">
+                <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_footer_classes', 'modal-footer'); ?>">
                     <?php foreach ($popup->buttons() as $button) : ?>
                         <a href="<?php echo esc_url($button['link']); ?>" class="btn btn-<?php echo esc_attr($button['theme']); ?>" target="<?php echo esc_attr($button['target']); ?>"><?php echo esc_html($button['label']); ?></a>
                     <?php endforeach; ?>
