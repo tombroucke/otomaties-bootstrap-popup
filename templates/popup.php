f1510cdbcee0df5fe413d343ee59c8603c19a06b
Tom Broucke <tom@tombroucke.be>
2024-10-14 15:45:54 +0200

    🩹 fix: Bootstrap 4 close behaviour

--- templates/popup.php
+++ templates/popup.php
@@ -4,7 +4,7 @@
             <div class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_header_classes', 'modal-header'); ?>">
                 <h5 class="<?php echo apply_filters('otomaties_bootstrap_popup_modal_title_classes', 'modal-title'); ?>" id="popup-<?php echo $popup->getId() ?>Title"><?php echo $popup->title(); ?></h5>
                 <?php if ($version == '4.x') : ?>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 <?php else : ?>
@@ -20,7 +20,11 @@
                         <a href="<?php echo esc_url($button['link']); ?>" class="btn btn-<?php echo esc_attr($button['theme']); ?>" target="<?php echo esc_attr($button['target']); ?>"><?php echo esc_html($button['label']); ?></a>
                     <?php endforeach; ?>
                     <?php if ($popup->showCloseButton()) : ?>
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal"><?php _e('Close', 'otomaties-popup'); ?></button>
+                        <?php if ($version == '4.x') : ?>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal"><?php _e('Close', 'otomaties-popup'); ?></button>
+                        <?php else : ?>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal"><?php _e('Close', 'otomaties-popup'); ?></button>
+                        <?php endif; ?>
                     <?php endif; ?>
                 </div>
             <?php endif; ?>
