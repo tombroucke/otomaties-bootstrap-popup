3cca2c1bc653fb0da23c142003669a86290c7362
Tom Broucke <tom@tombroucke.be>
2025-09-05 09:15:25 +0200

    ✨ feat: clear super page cache

--- app/Cache.php
+++ app/Cache.php
@@ -5,14 +5,21 @@ namespace Otomaties\BootstrapPopup;
 class Cache
 {
     public function cleanPost (array|string $postIds) {
-        foreach ($postIds as $postId) {
-            if (function_exists('rocket_clean_post')) {
+        $postUrls = array_map('get_permalink', (array)$postIds);
+        do_action("swcfpc_purge_cache", $postUrls);
+
+        if (function_exists('rocket_clean_post')) {
+            foreach ($postIds as $postId) {
                 rocket_clean_post($postId);
             }
         }
     }
 
-    public function cleanDomain(string $language = null) {
+    public function cleanDomain(?string $language = null) {
+        // Super Page Cache
+        do_action("swcfpc_purge_cache");
+
+        // WP Rocket
         if (function_exists('rocket_clean_domain')) {
             rocket_clean_domain($language);
         }
